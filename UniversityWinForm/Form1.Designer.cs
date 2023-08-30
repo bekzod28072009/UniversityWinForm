86d4c238e8670782260e77a2914181b22359efba
bekzod28072009 <bekhzodkeldiyorov@gmail.com>
2023-08-30 08:31:15 +0500

    Add project files.

---
+++ UniversityWinForm/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace UniversityWinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox1 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox2 = new PictureBox();
+            label3 = new Label();
+            pictureBox4 = new PictureBox();
+            label4 = new Label();
+            pictureBox5 = new PictureBox();
+            label5 = new Label();
+            pictureBox6 = new PictureBox();
+            label6 = new Label();
+            pictureBox7 = new PictureBox();
+            label7 = new Label();
+            pictureBox8 = new PictureBox();
+            label8 = new Label();
+            pictureBox9 = new PictureBox();
+            label9 = new Label();
+            pictureBox10 = new PictureBox();
+            panel2 = new Panel();
+            label10 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).BeginInit();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Control;
+            panel1.Location = new Point(301, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(10, 1150);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(2, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(136, 127);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(2, 172);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(71, 73);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(88, 215);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 30);
+            label1.TabIndex = 4;
+            label1.Text = "Home";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(88, 323);
+            label2.Name = "label2";
+            label2.Size = new Size(102, 30);
+            label2.TabIndex = 6;
+            label2.Text = "Student";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(2, 280);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(71, 73);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(88, 546);
+            label3.Name = "label3";
+            label3.Size = new Size(121, 30);
+            label3.TabIndex = 10;
+            label3.Text = "Professor";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(2, 503);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(71, 73);
+            pictureBox4.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox4.TabIndex = 9;
+            pictureBox4.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(88, 438);
+            label4.Name = "label4";
+            label4.Size = new Size(148, 30);
+            label4.TabIndex = 8;
+            label4.Text = "Department";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(2, 395);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(71, 73);
+            pictureBox5.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox5.TabIndex = 7;
+            pictureBox5.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(88, 760);
+            label5.Name = "label5";
+            label5.Size = new Size(66, 30);
+            label5.TabIndex = 14;
+            label5.Text = "Fees";
+            // 
+            // pictureBox6
+            // 
+            pictureBox6.Image = (Image)resources.GetObject("pictureBox6.Image");
+            pictureBox6.Location = new Point(2, 717);
+            pictureBox6.Name = "pictureBox6";
+            pictureBox6.Size = new Size(71, 73);
+            pictureBox6.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox6.TabIndex = 13;
+            pictureBox6.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(88, 652);
+            label6.Name = "label6";
+            label6.Size = new Size(91, 30);
+            label6.TabIndex = 12;
+            label6.Text = "Course";
+            // 
+            // pictureBox7
+            // 
+            pictureBox7.Image = (Image)resources.GetObject("pictureBox7.Image");
+            pictureBox7.Location = new Point(2, 609);
+            pictureBox7.Name = "pictureBox7";
+            pictureBox7.Size = new Size(71, 73);
+            pictureBox7.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox7.TabIndex = 11;
+            pictureBox7.TabStop = false;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(88, 982);
+            label7.Name = "label7";
+            label7.Size = new Size(102, 30);
+            label7.TabIndex = 18;
+            label7.Text = "Coleges";
+            // 
+            // pictureBox8
+            // 
+            pictureBox8.Image = (Image)resources.GetObject("pictureBox8.Image");
+            pictureBox8.Location = new Point(2, 939);
+            pictureBox8.Name = "pictureBox8";
+            pictureBox8.Size = new Size(71, 73);
+            pictureBox8.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox8.TabIndex = 17;
+            pictureBox8.TabStop = false;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label8.Location = new Point(88, 874);
+            label8.Name = "label8";
+            label8.Size = new Size(84, 30);
+            label8.TabIndex = 16;
+            label8.Text = "Salary";
+            // 
+            // pictureBox9
+            // 
+            pictureBox9.Image = (Image)resources.GetObject("pictureBox9.Image");
+            pictureBox9.Location = new Point(2, 831);
+            pictureBox9.Name = "pictureBox9";
+            pictureBox9.Size = new Size(71, 73);
+            pictureBox9.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox9.TabIndex = 15;
+            pictureBox9.TabStop = false;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Franklin Gothic Medium", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label9.Location = new Point(88, 1084);
+            label9.Name = "label9";
+            label9.Size = new Size(90, 30);
+            label9.TabIndex = 20;
+            label9.Text = "Logout";
+            // 
+            // pictureBox10
+            // 
+            pictureBox10.Image = (Image)resources.GetObject("pictureBox10.Image");
+            pictureBox10.Location = new Point(2, 1041);
+            pictureBox10.Name = "pictureBox10";
+            pictureBox10.Size = new Size(71, 73);
+            pictureBox10.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox10.TabIndex = 19;
+            pictureBox10.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ControlText;
+            panel2.BackgroundImageLayout = ImageLayout.None;
+            panel2.BorderStyle = BorderStyle.Fixed3D;
+            panel2.Controls.Add(label10);
+            panel2.Location = new Point(1230, 96);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(310, 926);
+            panel2.TabIndex = 21;
+            panel2.Paint += panel2_Paint;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.BackColor = SystemColors.ControlText;
+            label10.Font = new Font("Franklin Gothic Medium", 16F, FontStyle.Italic, GraphicsUnit.Point);
+            label10.ForeColor = SystemColors.Control;
+            label10.Location = new Point(41, 31);
+            label10.Name = "label10";
+            label10.Size = new Size(143, 37);
+            label10.TabIndex = 22;
+            label10.Text = "Statistics";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1599, 1148);
+            Controls.Add(panel2);
+            Controls.Add(label9);
+            Controls.Add(pictureBox10);
+            Controls.Add(label7);
+            Controls.Add(pictureBox8);
+            Controls.Add(label8);
+            Controls.Add(pictureBox9);
+            Controls.Add(label5);
+            Controls.Add(pictureBox6);
+            Controls.Add(label6);
+            Controls.Add(pictureBox7);
+            Controls.Add(label3);
+            Controls.Add(pictureBox4);
+            Controls.Add(label4);
+            Controls.Add(pictureBox5);
+            Controls.Add(label2);
+            Controls.Add(pictureBox2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox10).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox3;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox2;
+        private Label label3;
+        private PictureBox pictureBox4;
+        private Label label4;
+        private PictureBox pictureBox5;
+        private Label label5;
+        private PictureBox pictureBox6;
+        private Label label6;
+        private PictureBox pictureBox7;
+        private Label label7;
+        private PictureBox pictureBox8;
+        private Label label8;
+        private PictureBox pictureBox9;
+        private Label label9;
+        private PictureBox pictureBox10;
+        private Panel panel2;
+        private Label label10;
+    }
+}
---
+++ UniversityWinForm/Form1.cs
@@ -0,0 +1,15 @@
+namespace UniversityWinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
